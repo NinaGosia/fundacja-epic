d143d071a41cba6cb39958c4c482f6c151e76b77
Nina Daliva <ninadaliva@gmail.com>
2020-09-14 21:27:58 +0200

    * new section "archiwum"/"Dokumenty" * bugfix: navigation - scroll positions for anchors * cookie consent * styles: small improvements for "Wesprzyj nas" section

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,14 @@ var app = new Vue({
     el: "#app",
     data: {
         projektyIsCollapsed: false,
+        cookiesConsentIsAccepted: false,
+    },
+    created() {
+        this.cookiesConsentIsAccepted = (localStorage.cookiesConsent === 'true');
+    },
+    methods: {
+        acceptCookie() {
+            this.cookiesConsentIsAccepted = localStorage.cookiesConsent = true;
+        }
     }
 })
